d148d070a339995cd38f731da863905a74aa1407
Dave Pierre <davepierre@cmail.carleton.ca>
2020-04-20 00:42:03 -0400

    working now but will loop sometimes

--- Model/Common.py
+++ Model/Common.py
@@ -26,12 +26,9 @@ def heuristic(node,playerId):
     # this will evaluate how manyy players are currnt alive ohter then itself
     numberOfPlayers = 0
     gameBoard = node.state.board
-    for playerBoard  in gameBoard:
-        if(gameBoard[playerBoard].isPlayerAlive() and playerId != gameBoard[playerBoard].getId()):
-             numberOfPlayers +=1
 
-    if(not(gameBoard[playerId].isPlayerAlive())):
-        return -10000
+    if((gameBoard[playerId].isPlayerAlive())):
+        return gameBoard[playerId].getNumOfStickLeftHand() + gameBoard[playerId].getNumOfStickLeftHand()
 
     else:
         if(numberOfPlayers==1):
